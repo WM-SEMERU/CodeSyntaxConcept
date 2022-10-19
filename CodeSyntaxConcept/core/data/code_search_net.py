0117a28c595f4acbb27404dfbf5668017d4f70b2
Alejandro Velasco <svelascodimate@wm.edu>
2022-10-19 14:58:00 -0400

    fixed frequency count

--- CodeSyntaxConcept/core/data/code_search_net.py
+++ CodeSyntaxConcept/core/data/code_search_net.py
@@ -27,15 +27,38 @@ class CodeSearchNet:
         return testset_small, testset_medium, testset_large
 
     @staticmethod
+    def count_source_code_ast_type_frequency(source_code: str, language: str):
+        code_sample_node_types = TreeSitterParser.process_source_code(source_code, language)
+        token_counts = code_sample_node_types[code_sample_node_types.columns[0]].value_counts(dropna=True, sort=True)
+        node_type_counts = code_sample_node_types[code_sample_node_types.columns[1]].value_counts(dropna=True, sort=True)
+        parent_node_type_counts = code_sample_node_types[code_sample_node_types.columns[2]].value_counts(dropna=True, sort=True)
+        return token_counts, node_type_counts, parent_node_type_counts
+
+    @staticmethod
+    def transform_code_counts_to_dataframe(total_token_counts: pd.Series, total_node_type_counts: pd.Series, total_parent_node_type_counts: pd.Series):
+        ## total token frequency counts
+        df_total_token_counts = total_token_counts.to_frame()
+        df_total_token_counts.reset_index(inplace=True)
+        df_total_token_counts.columns = ['token', 'counts']
+        ## total node type frequency counts
+        df_total_node_type_counts = total_node_type_counts.to_frame()
+        df_total_node_type_counts.reset_index(inplace=True)
+        df_total_node_type_counts.columns = ['node_type', 'counts']
+        ## total parent node type frequency counts
+        df_total_parent_node_type_counts = total_parent_node_type_counts.to_frame()
+        df_total_parent_node_type_counts.reset_index(inplace=True)
+        df_total_parent_node_type_counts.columns = ['parent_type', 'counts']
+        return df_total_token_counts, df_total_node_type_counts, df_total_parent_node_type_counts
+        
+
+    @staticmethod
     def count_ast_type_frequency(test_set):
-        node_type_counts = pd.Series()
-        parent_node_type_counts = pd.Series()
-        token_counts = pd.Series()
-        for code_sample in test_set: 
-            code_sample_node_types = TreeSitterParser.process_source_code(code_sample['whole_func_string'], code_sample['language'])
-            token_counts = token_counts.add(code_sample_node_types[code_sample_node_types.columns[0]].value_counts(), fill_value=0)
-            node_types = code_sample_node_types[code_sample_node_types.columns[1]].value_counts()
-            parent_types = code_sample_node_types[code_sample_node_types.columns[2]].value_counts()
-            node_type_counts = node_type_counts.add(node_types, fill_value=0)
-            parent_node_type_counts = parent_node_type_counts.add(parent_types, fill_value=0)
-        return token_counts.rename_axis('token').to_frame('counts'), node_type_counts.rename_axis('node_type').to_frame('counts'), parent_node_type_counts.rename_axis('parent_node_type').to_frame('counts')
+        total_token_counts = pd.Series(dtype='float64')
+        total_node_type_counts = pd.Series(dtype='float64')
+        total_parent_node_type_counts = pd.Series(dtype='float64')
+        for code_sample in test_set:
+            token_counts, node_type_counts, parent_node_type_counts = CodeSearchNet.count_source_code_ast_type_frequency(code_sample['whole_func_string'], code_sample['language'])
+            total_token_counts = total_token_counts.add(token_counts, fill_value=0)
+            total_node_type_counts = total_node_type_counts.add(node_type_counts, fill_value=0)
+            total_parent_node_type_counts = total_parent_node_type_counts.add(parent_node_type_counts, fill_value=0) 
+        return CodeSearchNet.transform_code_counts_to_dataframe(total_token_counts, total_node_type_counts, total_parent_node_type_counts)
