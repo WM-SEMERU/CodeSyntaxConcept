35e76f1ef64d180f3527846a00ebf41c7ea9ee2f
Alejandro Velasco <svelascodimate@wm.edu>
2023-02-27 08:34:39 -0500

    updated

--- scripts/code-syntax-concept.py
+++ scripts/code-syntax-concept.py
@@ -9,24 +9,26 @@ from CodeSyntaxConcept.evaluator import Evaluator
 #checkpoint = "EleutherAI/gpt-neo-125M"
 #checkpoint = "EleutherAI/gpt-neo-1.3B"
 #checkpoint = "EleutherAI/gpt-neo-2.7B"
-#checkpoint = "microsoft/CodeGPT-small-py"
+checkpoint = "microsoft/CodeGPT-small-py"
 #checkpoint = "microsoft/CodeGPT-small-py-adaptedGPT2"
 #checkpoint = "Salesforce/codegen-16B-multi"
 #checkpoint = "Salesforce/codegen-6B-multi"
 #checkpoint = "Salesforce/codegen-2B-multi"
-checkpoint = "himanshu-dutta/pycoder-gpt2"
+#checkpoint = "himanshu-dutta/pycoder-gpt2"
 
 language = "python"
 maximun_number_of_samples = 10000
+sequence_max_size = 1024
 save_path = "output/"
 
 ######### YOU NEED TO SET THIS FIRST #########
 evaluator = Evaluator(checkpoint, language)
-print('maximun number of tokens per sequence: '+str(evaluator.tokenizer.tokenizer.max_len_single_sentence))
+print('maximun number of tokens per sequence: '+str(sequence_max_size))
 
-test_set = utils.get_random_sub_set_test_set(utils.get_test_sets(load_dataset("code_search_net", split='test'), language, evaluator.tokenizer.tokenizer.max_len_single_sentence, evaluator.tokenizer), maximun_number_of_samples)
+#test_set = utils.get_random_sub_set_test_set(utils.get_test_sets(load_dataset("code_search_net", split='test'), language, evaluator.tokenizer.tokenizer.max_len_single_sentence, evaluator.tokenizer), maximun_number_of_samples)
+test_set = utils.get_random_sub_set_test_set(utils.get_test_sets(load_dataset("code_search_net", split='test'), language, sequence_max_size, evaluator.tokenizer), maximun_number_of_samples)
 testset_concepts = evaluator(test_set)
 
 print(testset_concepts.describe())
-testset_concepts.to_csv(save_path+"codesearch_tesbed_"+checkpoint.replace("/","-")+"_"+str(maximun_number_of_samples)+".csv")
+testset_concepts.to_csv(save_path+"codesearch_tesbed_"+checkpoint.replace("/","-")+"_"+str(sequence_max_size)+"_"+str(maximun_number_of_samples)+".csv")
 
--- scripts/commands.md
+++ scripts/commands.md
@@ -1,4 +1,10 @@
 #### TABLES
 
-nohup python3 code-syntax-concept.py > logs/base_tesbed_gpt-neo-125M.txt &
-nohup python3 code-syntax-concept.py > logs/base_tesbed_gpt-neo-1.3B.txt &
+nohup python3 -u code-syntax-concept.py > logs/gpt-neo-125M.txt &
+nohup python3 -u code-syntax-concept.py > logs/gpt-neo-1.3B.txt &
+nohup python3 -u code-syntax-concept.py > logs/codegpt-small-py-adaptedGPT2.txt &
+nohup python3 -u code-syntax-concept.py > logs/codegpt-small-py.txt &
+nohup python3 -u code-syntax-concept.py > logs/codegen-16B-multi.txt &
+nohup python3 -u code-syntax-concept.py > logs/codegen-6B-multi.txt &
+nohup python3 -u code-syntax-concept.py > logs/codegen-2B-multi.txt &
+nohup python3 -u code-syntax-concept.py > logs/pycoder-gpt2.txt &
